6eab4b69227b5f27acb36e27710c708ff6f73996
Jannis Bolzernr <jannis.bolzern@gmail.com>
2025-04-22 15:04:18 +0200

    Cleaned up SSF script

--- SSF.R
+++ SSF.R
@@ -1,212 +1,174 @@
-# Load necessary packages
-library(amt)
-library(lubridate)
-library(dplyr)
-library(sf)
-library(raster)
-library(tidyverse)
-library(leaflet)
-library(geosphere)
-library(terra)
-library(glmmTMB)
-library(DHARMa)
-library(emmeans)
+# Load necessary packages -----------------------------------------------------
+pacman::p_load(
+  tidyverse,
+  amt,
+  sf,
+  geosphere,
+  terra,
+  glmmTMB,
+  DHARMa,
+  emmeans
+)
 
-options(scipen = 999) # turn off scientific notation
-options(digits = 15) # set digits to 15 to ensure GPS coordinates aren't truncated
-sf_use_s2(FALSE) # workaround for one of the amt functions 
+options(scipen = 999)
+options(digits = 15)
 
-# Read, rename, select, and filter tracking data
-tracking_data <- read_delim("data/bobcat_coyotes_wa_gps.csv") %>%
+# Load and prepare tracking data ---------------------------------------------
+tracking_data <- read_delim("data/bobcat_coyotes_wa_gps.csv") |> 
   dplyr::rename(
-    id = `event-id`,
     long = `location-long`, 
     lat = `location-lat`,
-    animal_id = `individual-local-identifier`,
-    species = `individual-taxon-canonical-name`
-  ) %>%
-  dplyr::arrange(animal_id, timestamp) %>%
-  dplyr::select(animal_id, species, timestamp, lat, long) %>%
+    id = `individual-local-identifier`,
+    timestamp = `timestamp`,
+    species = `individual-taxon-canonical-name`) |> 
+  dplyr::arrange(id, timestamp) |> 
+  dplyr::select(id, species, timestamp, lat, long) |> 
   dplyr::filter(
-    !animal_id %in% c(
+    !id %in% c(
       "MVBOB87M", "NECOY12F", "MVCOY65M", "MVBOB88M", 
       "NEBOB8M", "MVBOB77M", "MVBOB91M", "NEBOB11M", 
-      "NECOY42F", "MVCOY72F", "NECOY14M"
-    ),
-    !(animal_id == "MVBOB71M" & timestamp > as.POSIXct("2019-09-24 00:00:00"))
+      "NECOY42F", "MVCOY72F", "NECOY14M"),
+    !(id == "MVBOB71M" & timestamp > as.POSIXct("2019-09-24 00:00:00"))
   )
 
-
-# plot a random subset of the locations (to reduce processing time)
-tracking_for_plot <- tracking_data %>% mutate(rand = round(runif(nrow(tracking_data), 0, 5)), mock = 1) %>% filter(rand == 1) 
-
-cols25 <- function() {
-  c("#1f78b4", "#33a02c", "#e31a1c", "#ff7f00", "#6a3d9a", 
-    "#a6cee3", "#b2df8a", "#fb9a99", "#fdbf6f", "#cab2d6",
-    "#ffff99", "#b15928", "#8dd3c7", "#fb8072", "#80b1d3",
-    "#bebada", "#ffed6f", "#fdb462", "#b3de69", "#fccde5",
-    "#d9d9d9", "#bc80bd", "#ccebc5", "#ffed6f", "#7FC97F")
-}
-
-leaflet() %>% 
-  addProviderTiles(providers$Esri.NatGeoWorldMap) %>%
-  addCircles(data = tracking_data , 
-             color = ~ colorFactor(cols25(), domain = factor(animal_id))(factor(animal_id)),
-             label = paste(tracking_data$animal_id,
-                           tracking_data$timestamp), opacity = 1) 
-
-
-# Select variables, nest by individual animal, and make 'amt' track
-track <- tracking_data %>%
-  data.frame() %>%
-  nest(data = c(-animal_id, -species)) %>%
-  mutate(trk = lapply(data, function(d) {
-    make_track(d, .x = long, .y = lat, .t = timestamp, crs = 4326)
-  }))
-
+# Create and inspect tracks ---------------------------------------------------
+track <- tracking_data |>
+  nest(data = c(-id, -species)) |>
+  mutate(trk = map(data, ~ make_track(.x, long, lat, timestamp, crs = 4326)))
 
 # Summarize sampling rate
-trackSummary <- track %>% mutate(sr = lapply(trk, summarize_sampling_rate, time_unit = "hour")) %>%
-  dplyr::select(animal_id, sr) %>% unnest %>% 
-  left_join(distinct(data.frame(track)[,c("animal_id", "species")])) %>%
+trackSummary <- track |> 
+  mutate(sr = lapply(trk, summarize_sampling_rate, time_unit = "hour")) |> 
+  dplyr::select(id, sr) |> 
+  unnest(cols = sr) |>
+  left_join(distinct(dplyr::select(tracking_data, id, species))) |>
   arrange(species, median)
-print(trackSummary, n = 70)
 
 # Get the individual sampling rates instead of the summary, for plotting.
 trackSummarySamples <- track |>
   mutate(sr = lapply(trk, summarize_sampling_rate, time_unit = "hour", summarize = FALSE)) |>
-  dplyr::select(animal_id, sr) |>
-  unnest(cols = c(sr)) |>
-  mutate(species = ifelse(grepl("BOB", animal_id), "Bobcat", "Coyote"))
-png("img/bobcat_coyote_sampling_rates.png", width = 800, height = 600)
+  dplyr::select(id, sr) |>
+  unnest(cols = sr) |>
+  mutate(species = ifelse(grepl("BOB", id), "Bobcat", "Coyote"))
+
 boxplot(sr ~ species, outline = FALSE, data = trackSummarySamples, 
         xlab = "", 
         ylab = "Sampling interval in hours, with outliers removed")
-dev.off()
 
-# separate by species
-coyote <- track %>% filter(grepl("COY", animal_id))
-bobcat <- track %>% filter(grepl("BOB", animal_id))
+# Split into species ----------------------------------------------------------
+coyote <- filter(track, grepl("COY", id))
+bobcat <- filter(track, grepl("BOB", id))
 
-coyote1 <- coyote %>%
-  mutate(stp = map(trk, function(x)
-    x %>% track_resample(rate = hours(4), tolerance = minutes(10)) %>%
-      steps_by_burst() %>% 
+# Step generation for SSF -----------------------------------------------------
+coyote1 <- coyote |> 
+  mutate(stp = map(trk, function(df)
+    df |> 
+      track_resample(rate = hours(4), tolerance = minutes(10)) |> 
+      steps_by_burst() |> 
       random_steps(n_control = 10) %>% 
-      mutate(log_sl_ = log(sl_ + 1), cos_ta_ = cos(ta_)))) %>%  # Distribute 10 available points for each used location
-  dplyr::select(-data, -trk) %>%
-  unnest(cols = stp) %>%
-  mutate(case_binary = ifelse(case_ == TRUE, 1, 0))
+      mutate(log_sl_ = log(sl_ + 1), cos_ta_ = cos(ta_)))) |> 
+  dplyr::select(-data, -trk) |> 
+  unnest(cols = stp) |> 
+  mutate(case_binary_ = ifelse(case_ == TRUE, 1, 0))
 
-bobcat1 <- bobcat %>%
-  mutate(stp = map(trk, function(x)
-    x %>% track_resample(rate = hours(4), tolerance = minutes(10)) %>%
-      steps_by_burst() %>% 
-      random_steps(n_control = 10) %>% 
-      mutate(log_sl_ = log(sl_ + 1), cos_ta_ = cos(ta_)))) %>%  # Distribute 10 available points for each used location
-  dplyr::select(-data, -trk) %>%
-  unnest(cols = stp) %>%
-  mutate(case_binary = ifelse(case_ == TRUE, 1, 0))
+bobcat1 <- bobcat |> 
+  mutate(stp = map(trk, function(df)
+    df |> 
+      track_resample(rate = hours(8), tolerance = minutes(10)) |> 
+      steps_by_burst() |>  
+      random_steps(n_control = 10) |> 
+      mutate(log_sl_ = log(sl_ + 1), cos_ta_ = cos(ta_)))) |> 
+  dplyr::select(-data, -trk) |> 
+  unnest(cols = stp) |> 
+  mutate(case_binary_ = ifelse(case_ == TRUE, 1, 0))
 
-write_csv(coyote1, "data/coyote_extracted.csv")
-write_csv(bobcat1, "data/bobcat_extracted.csv")
+# Export resampled step data
+write_csv(coyote1, "data/coyote_resampled.csv")
+write_csv(bobcat1, "data/bobcat_resampled.csv")
 
-coyote_extracted <- read_delim("data/coyote_extracted.csv") %>%
-  dplyr::select(-log_sl_, -sl_) %>%
-  mutate(# step lengths from amt are in degrees; overwrite to meters
-    sl_ = distGeo(.[,c("x1_","y1_")],.[,c("x2_","y2_")]),
-    log_sl_ = log(sl_),
-    # instead of using calendar years, we start the year on 1 Dec of the prior year
-    analysis_year = ifelse(month(t2_) == 12, year(t2_) + 1, year(t2_)),
-    season = ifelse(month(t2_) %in% 4:11, "summer", "winter")) 
+# Read and recalculate step length (in meters) --------------------------------
+recalc_steps <- function(file) {
+  read_delim(file) |>
+    dplyr::select(-log_sl_, -sl_) |>
+    mutate(
+      sl_ = distGeo(across(c(x1_, y1_)), across(c(x2_, y2_))),
+      log_sl_ = log(sl_)
+    )
+}
 
-bobcat_extracted <- read_delim("data/bobcat_extracted.csv") %>%
-  dplyr::select(-sl_) %>%
-  mutate(# step lengths from amt are in degrees; overwrite to meters
-    sl_ = distGeo(.[,c("x1_","y1_")],.[,c("x2_","y2_")]),
-    log_sl_ = log(sl_),
-    # instead of using calendar years, we start the year on 1 Dec of the prior year
-    analysis_year = ifelse(month(t2_) == 12, year(t2_) + 1, year(t2_)),
-    season = ifelse(month(t2_) %in% 4:11, "summer", "winter")) 
+coyote_resampled <- recalc_steps("data/coyote_resampled.csv")
+bobcat_resampled <- recalc_steps("data/bobcat_resampled.csv")
 
-
-# Load and preprocess rasters --------------------------------------------------
+# Load and prepare rasters ----------------------------------------------------
 hfp <- rast("data/HFP_washington.tif")
 NAflag(hfp) <- 64536  # Set no-data value
 hfp_capped <- classify(hfp, matrix(c(50000, Inf, 50000), ncol = 3, byrow = TRUE)) # Cap at 50k
 hfp_scaled <- app(hfp_capped, \(x) (x / 50000) - 0.5)  # Scale to -0.5–0.5
-                       
+
 land_use <- rast("data/ESA_washington.tif")
-                       
+
 # Land use class labels (ESA WorldCover 2021)
-class_labels <- c(
-  "10" = "Tree_cover", "20" = "Shrubland", "30" = "Grassland",
-  "40" = "Cropland", "50" = "Built_up", "60" = "Bare_sparse_vegetation",
-  "70" = "Snow_and_ice", "80" = "Permanent_water_bodies",
-  "90" = "Herbaceous_wetland", "95" = "Mangroves", "100" = "Moss_and_lichen"
+esa_labels <- c(
+  "10" = "Tree cover", "20" = "Shrubland", "30" = "Grassland",
+  "40" = "Cropland", "50" = "Built-up", "60" = "Bare or sparse vegetation",
+  "70" = "Snow and ice", "80" = "Permanent water bodies",
+  "90" = "Herbaceous wetland", "95" = "Mangroves", "100" = "Moss and lichen"
 )
 
-# Extract covariates -----------------------------------------------------------
-extract_covariates <- function(data) {
-  data %>%
+# Extract covariates from rasters ---------------------------------------------
+extract_covariates <- function(df) {
+  df |>
     mutate(
       human_footprint = terra::extract(hfp_scaled, cbind(x2_, y2_))[, 1],
-      land_use_code = terra::extract(land_use, cbind(x2_, y2_))[, 1]
-    ) %>%
-    mutate(
-      land_use = factor(land_use_code, 
-                        levels = names(class_labels),
-                        labels = class_labels)
+      land_use_code = terra::extract(land_use, cbind(x2_, y2_))[, 1],
+      land_use = factor(land_use_code, levels = names(esa_labels), labels = esa_labels)
     )
 }
-  
-coyote_extracted1 <- extract_covariates(coyote_extracted)
-bobcat_extracted1 <- extract_covariates(bobcat_extracted)
 
-# Final formatting --------------------------------------------------------
-format_for_ssf <- function(data) {
-  data %>%
+coyote_cov <- extract_covariates(coyote_resampled)
+bobcat_cov <- extract_covariates(bobcat_resampled)
+
+# Final formatting for SSF ----------------------------------------------------
+prepare_ssf_data <- function(df) {
+  df |>
     mutate(
-      step_id_ = paste(animal_id, step_id_, sep = "_")) %>% # Unique stratum ID
-    group_by(animal_id) %>%
-    mutate(n = n()/11) %>%  # 1 used + 10 available steps per stratum
+      land_use = as.factor(land_use),
+      land_use_grouped = fct_collapse(
+        land_use,
+        "TreeCover" = "Tree cover",
+        "Open"      = c("Grassland", "Bare or sparse vegetation", "Moss and lichen"),
+        "Cropland"  = "Cropland",
+        "BuiltUp"   = "Built-up",
+        "Water"     = c("Permanent water bodies", "Herbaceous wetland")
+      ),
+      step_id_ = paste(id, step_id_, sep = "_") # Unique stratum ID
+    ) |>
+    group_by(id) |>
+    mutate(n = n() / 11) |> # 1 used + 10 available steps per stratum
     ungroup()
 }
 
-coyote_final <- format_for_ssf(coyote_extracted1)
-bobcat_final <- format_for_ssf(bobcat_extracted1)
+coyote_final <- prepare_ssf_data(coyote_cov)
+bobcat_final <- prepare_ssf_data(bobcat_cov)
 
+# Save final data
 write_csv(coyote_final, "data/coyote_ssf_data.csv")
 write_csv(bobcat_final, "data/bobcat_ssf_data.csv")
 
-# Coyote Step Selection Function ---------------------------------------------
+# Fit SSF: Coyote -------------------------------------------------------------
+# Read SSF ready data
+coyote_ssf_data <- read_delim("data/coyote_ssf_data.csv")
 
-coyote_final <- read_delim("data/coyote_ssf_data.csv") 
-
-table(coyote_final$land_use)
-
-coyote_final <- coyote_final |> 
-  mutate(land_use_grouped = fct_collapse(
-    land_use,
-    "Forest" = "Tree cover",
-    "Open_Natural" = c("Grassland", "Shrubland"),
-    "Cropland" = "Cropland",
-    "Built_up" = "Built-up",
-    "Sparse" = c("Bare/sparse vegetation", "Moss and lichen"),
-    "Wet" = c("Permanent water bodies", "Herbaceous wetland")
-  ))
-
-# Define number of CPU cores
 nt <- parallel::detectCores() - 2
 
 ssf_coyote_struc <- glmmTMB(
-  case_binary ~ -1 + 
-    land_use * human_footprint +              # full interaction
-    offset(log_sl_) +
-    (1 | step_id_) +
-    (0 + land_use + human_footprint || animal_id),
+  case_binary_ ~ -1 + 
+    land_use_grouped * human_footprint + 
+    offset(log_sl_) + 
+    (1 | step_id_) + 
+    (0 + land_use_grouped + human_footprint || id),
   family = poisson,
-  data = coyote_final,
+  data = coyote_ssf_data,
   control = glmmTMBControl(parallel = nt),
   doFit = FALSE
 )
@@ -214,10 +176,64 @@ ssf_coyote_struc <- glmmTMB(
 # Check theta values (to build map/start correctly)
 length(ssf_coyote_struc$parameters$theta)
 
-ssf_coyote_struc$parameters$theta[1] = log(1e3) 
-ssf_coyote_struc$mapArg = list(theta=factor(c(NA,1:9)))
+ssf_coyote_struc$parameters$theta[1] <- log(1e3)
+ssf_coyote_struc$mapArg <- list(theta = factor(c(NA, 1:6)))
 ssf_coyote <- glmmTMB:::fitTMB(ssf_coyote_struc)
 
-summary(ssf_coyote)
+#saveRDS(ssf_coyote, file = "models/ssf_coyote_model.rds")
+#ssf_coyote <- readRDS("models/ssf_coyote_model.rds")
 
+summary(ssf_coyote)
 confint(ssf_coyote)
+
+# Marginal effects of HFP by land cover
+trends_coyote <- emtrends(ssf_coyote, ~ land_use_grouped, var = "human_footprint") |>
+  summary(infer = c(TRUE, TRUE))
+
+ggplot(trends_coyote, aes(x = land_use_grouped, y = human_footprint.trend)) +
+  geom_point() +
+  geom_errorbar(aes(ymin = asymp.LCL, ymax = asymp.UCL), width = 0.2) +
+  geom_hline(yintercept = 0, linetype = "dashed") +
+  ylab("Effect of Human Footprint") +
+  xlab("Land Cover Type") +
+  theme_minimal()
+
+# Fit SSF: Bobcat -------------------------------------------------------------
+# Read SSF ready data
+bobcat_ssf_data <- read_delim("data/bobcat_ssf_data.csv")
+
+# Dropping land use groups with too few used steps
+# (virtually no variation in case status)
+bobcat_ssf_filtered <- bobcat_ssf_data |> 
+  filter(!(land_use_grouped %in% c("BuiltUp", "Water", "Cropland")))
+
+nt <- parallel::detectCores() - 2
+
+ssf_bobcat_struc <- glmmTMB(
+  case_binary_ ~ -1 + 
+    land_use_grouped * human_footprint + 
+    offset(log_sl_) + 
+    (1 | step_id_) + 
+    (0 + land_use_grouped + human_footprint || id),
+  family = poisson,
+  data = bobcat_ssf_filtered,
+  control = glmmTMBControl(parallel = nt),
+  doFit = FALSE
+)
+
+# Check theta values (to build map/start correctly)
+length(ssf_bobcat_struc$parameters$theta)
+
+ssf_bobcat_struc$parameters$theta[1] <- log(1e3)
+ssf_bobcat_struc$mapArg <- list(theta = factor(c(NA, 1:3)))
+ssf_bobcat <- glmmTMB:::fitTMB(ssf_bobcat_struc)
+
+#saveRDS(ssf_bobcat, file = "models/ssf_bobcat_model.rds")
+#ssf_bobcat <- readRDS("models/ssf_coyote_model.rds")
+
+summary(ssf_bobcat)
+confint(ssf_bobcat)
+
+# Marginal trends for bobcats
+trends_bobcat <- emtrends(ssf_bobcat, ~ land_use_grouped, var = "human_footprint") |> 
+  summary(infer = c(TRUE, TRUE))
